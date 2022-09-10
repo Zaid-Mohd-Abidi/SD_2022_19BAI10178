cf2ba76267b289e0f7115101bf6e4e531af803f1
zaid <64187039+Zaid-Mohd-Abidi@users.noreply.github.com>
2022-09-10 18:01:53 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hitwicket-Game
+//
+//  Created by Zaid Mohd Abidi on 10/09/22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int p1,p2;
+    cout<<"Player1 Input: ";
+    cin>>p1;
+    
+    return 0;
+}
